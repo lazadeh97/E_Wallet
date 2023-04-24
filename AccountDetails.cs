69b2f2e8da5ceb06704f83cc2b4e128c46487fc1
lazadeh97 <126696851+lazadeh97@users.noreply.github.com>
2023-04-24 23:44:33 +0400

    E-Wallet created.

---
+++ AccountDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Wallet
+{
+    internal class AccountDetails:Account
+    {
+        private int accountNo { get; set; }
+        public int AccountNo
+        {
+            get { return accountNo; }
+            set { accountNo = AccountId; }
+        }
+
+        public double balance { get; set; }
+        public string currency { get; set; }
+        public string description { get; set; }
+        public bool isActive { get; set; }
+    }
+}
